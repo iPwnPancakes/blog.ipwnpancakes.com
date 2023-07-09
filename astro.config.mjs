5a9e831664fa9c07c33fecbf530601f77c1c1bb0
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2023-07-09 00:55:34 -0500

    Got astro and storyblok working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,34 @@
 import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import tailwind from '@astrojs/tailwind';
+import { loadEnv } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+const env = loadEnv('', process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: 'hybrid',
+    integrations: [
+        storyblok({
+            accessToken: env.STORYBLOK_TOKEN,
+            apiOptions: {
+                region: 'us'
+            },
+            components: {
+                post: 'storyblok/Post',
+                page: 'storyblok/Page',
+                feature: 'storyblok/Feature',
+                grid: 'storyblok/Grid',
+                teaser: 'storyblok/Teaser',
+            }
+        }),
+        tailwind()
+    ],
+    vite: {
+        plugins: [basicSsl()],
+        server: {
+            https: true
+        }
+    }
+});
