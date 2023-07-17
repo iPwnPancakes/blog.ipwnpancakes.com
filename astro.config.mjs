9c1abcbd22c81b74ba52b1ff14abdf6b3439d6db
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2023-07-16 23:40:01 -0500

    Using tailwind typography in project

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,6 +21,7 @@ export default defineConfig({
                 feature: 'storyblok/Feature',
                 grid: 'storyblok/Grid',
                 teaser: 'storyblok/Teaser',
+                textSection: 'storyblok/TextSection',
             }
         }),
         tailwind()
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,5 +6,26 @@ module.exports = {
 			space: {}
 		},
 	},
-	plugins: [],
+	daisyui: {
+		themes: [
+			'dracula',
+			{
+				mytheme: {
+					"primary": "#8dd861",
+					"secondary": "#9ccde2",
+					"accent": "#94ecfc",
+					"neutral": "#211c27",
+					"base-100": "#2a4150",
+					"info": "#a4d1ea",
+					"success": "#57eac5",
+					"warning": "#e7851d",
+					"error": "#ed6a5e",
+				},
+			},
+		],
+	},
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('daisyui')
+	],
 }
